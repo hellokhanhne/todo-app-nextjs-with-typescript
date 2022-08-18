77ff4238a5a0429ce394aad012f362265de9fd39
Nguyen Ngoc Khanh <84921391+hellokhanhne@users.noreply.github.com>
2022-08-18 13:26:05 +0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  
+  return (
+    <Container maxWidth="lg">
+      <Box></Box>
+    </Container>
+  );
+};
+
+export default Home;
